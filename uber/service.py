ab379e94cc5d8f88c5ed3002a5bfd0218e9271e3
V3NKY <dr.venky@docsapp.in>
2021-01-22 17:37:18 +0530

    implement  rider service methods - ride started, accepted, ride ended and Distance cal service

--- uber/service.py
+++ uber/service.py
@@ -1,11 +1,31 @@
+from math import sqrt
+from datetime import datetime
 from .models import  User, Driver, Pending_Ride, User_Ride
+
+class Distance_Service:
+    def calc_two_location_distance(self, location1, location2):
+        lat1 = location1.latitude
+        long1 = location1.longitude
+        lat2 = location2.latitude
+        long2 = location2.longitude
+        distance = sqrt(((lat2 - lat1) ** 2) + ((long2 - long1) ** 2))
+        rounded_distance = round(distance, 2)
+        return round_distance
+
+
+    def calc_distance_from_origin(self, location):
+        distance = sqrt((location.latitude ** 2) + (location.longitude ** 2))
+        rounded_distance = round(distance, 2)
+        return round_distance
+
+
 
 class Driver_Service:
     def __init__(self, userId, location, cab_color):
         self.userId = userId
         self.cab_color = cab_color
-        self.latitude = location.latitude
-        self.longitude = location.longitude
+        self.user_latitude = location.latitude
+        self.user_longitude = location.longitude
 
     def get_available_drivers(self):
         available_drivers = 100
@@ -18,26 +38,50 @@ class Driver_Service:
             nearest_driver = 124
             return nearest_driver
 
+    def process_ride_request(self):
+        nearest_driver = self.get_nearest_driver()
+        if nearest_driver:
+            rideId = uuid4()
+            User_Ride.objects.create(user_id=self.userId, driver_id=nearest_driver, ride_id=rideId, ride_state="Pending",
+                                     ride_request_time=datetime.now())
+            rideObj = User_Ride.get(ride_id=rideId)
+            Push_Notification.notify_driver(nearest_driver, rideId)
+            return {success: True, ride_data: rideObj}
+        return {success: False, message: "NO DRIVERS AVAILABLE, Pls try after sometime" }
+
+
 
 class Ride_Service:
     def __init__(self, rideId):
         self.rideId = rideId
         self.rideObj = User_Ride.objects.get(ride_id=rideId)
+        self.driverId = self.rideObj.driver_id
 
     def ride_accepted(self):
-        driverId = self.rideObj.driver_id
         userId = self.rideObj.user_id
-        driverObj = Driver.objects.get(driver_id=driverId)
-        User_Ride.objects.filter(driver_id=driverId).update(available=False)
+        driverObj = Driver.objects.get(driver_id=self.driverId)
+        Driver.objects.filter(driver_id=self.driverId).update(available=False)
+        User_Ride.objects.filter(ride_id=self.rideId).update(ride_status="Accepted")
         Push_Notification.notify_user(userId, driverObj)
         return True
 
 
-    def ride_started(self):
-        pass
+    def ride_started(self, start_location):
+        start_lat = start_location.latitude
+        start_long = start_location.longitude
+        start_time = datetime.now()
+        ride_status = "Started"
 
-    def ride_ended(self):
-        pass
+
+    def ride_ended(self, dest_location):
+        dest_lat = dest_location.latitude
+        dest_long = dest_location.longitude
+        Driver.objects.filter(driver_id=self.driverId).update(available=True, latest_lat=dest_lat, latest_long=dest_long)
+        dest_time = datetime.now()
+        travel_time = dest_time - self.rideObj.start_time
+        travel_time_min = round(travel_time.seconds / 60 , 2)
+        ride_status = "Ended"
+
 
 
 
